2d638f466dee363a01a193d7b9bca229bcdd2600
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2019-04-27 13:42:18 -0500

    added sqlite orm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
@@ -9,12 +10,20 @@
 
 
 use rocket_contrib::json::Json;
+use rocket_contrib::databases::diesel;
+
+
+#[database("sqlite_db")]
+struct DbConn(diesel::SqliteConnection);
+
+
+type SensorID = u64;
 
 
 #[derive(Serialize, Deserialize, Debug)]
 struct GardenData {
-    sensor_id: u64,
-    moisture_level: u32
+    sensor_id: SensorID,
+    moisture_content: u8
 }
 
 
@@ -23,21 +32,46 @@ fn http_ok(msg: &String) -> String {
 }
 
 
+fn http_bad_request(msg: &String) -> String {
+    format!("HTTP/1.1 400 BAD REQUEST \r\n\r\n{}\r\n", msg)
+}
+
+
+#[get("/can-i-water/<sensor_id>")]
+fn can_i_water(sensor_id: SensorID) -> String {
+    if true {
+        http_ok(&String::from("yes"))
+    } else {
+        http_ok(&String::from("no"))
+    }
+}
+
+
 #[get("/")]
 fn hello() -> String {
-    let msg = String::from("Welcome to KloverTech SmartGarden\n");
+    let msg = String::from("Welcome to KloverTech SmartGarden");
     http_ok(&msg)
 }
 
 
 #[post("/log", format="Application/json", data="<data>")]
-fn log(data: Json<GardenData>) -> String {
-    let msg = format!("sensor #{} has moister level {}", data.sensor_id,
-                                                         data.moisture_level);
-    http_ok(&msg)
+fn log(conn: DbConn, data: Json<GardenData>) -> String {
+    if data.moisture_content > 100 {
+        let msg = String::from(
+            "moisture_content must be an integer between 0 to 100");
+        http_bad_request(&msg)
+    } else {
+        let msg = format!(
+            "sensor #{} has moister content {}", data.sensor_id,
+                                                 data.moisture_content);
+        http_ok(&msg)
+    }
 }
 
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello, log]).launch();
+    rocket::ignite()
+        .mount("/", routes![hello, log, can_i_water])
+        .attach(DbConn::fairing())
+        .launch();
 }
